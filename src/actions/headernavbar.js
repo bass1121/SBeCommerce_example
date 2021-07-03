4f83a70c8f8de2e9194e7b39b534bf58bc4b867f
Sean Bass <seanbass99@gmail.com>
2021-07-03 02:55:00 -0500

    finished navbar feature

--- src/actions/headernavbar.js
+++ src/actions/headernavbar.js
@@ -1,8 +1,8 @@
 import {
     SET_HEADER_LINKS,
-    SET_NAVBAR_LINKS
+    SET_NAVBAR_LINKS,
+    CHANGE_NAVBAR_ACTIVE
 } from './types';
-
 
 export function setHeaderLinks(links) {
     return ({
@@ -10,10 +10,16 @@ export function setHeaderLinks(links) {
         payload: links
     })
 }
-
 export function setNavbarLinks(links) {
     return ({
         type: SET_NAVBAR_LINKS,
         payload: links
+    })
+}
+
+export function changeNavbarActive(_id) {
+    return ({
+        type: CHANGE_NAVBAR_ACTIVE,
+        payload: _id
     })
 }
--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,21 +1,30 @@
-import React, { Component } from "react";
-import SignUpForm from "./signUpForm";
-import PageTitle from "../pageTitle";
+import React, { Component } from 'react';
+import SignUpForm from './signupForm';
+import PageTitle from '../pageTitle';
+
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
 class SignUp extends Component {
 
-    onSubmit= (fields) => {
-        console.log(fields);
+    componentDidMount() {
+        this.props.setHeaderLinks([]);
+        this.props.setNavbarLinks([]);
     }
 
+    onSubmit = (fields) => {
+        console.log(fields);
+    }
     render() {
         return (
-            <div className="sign-up">
-                <PageTitle className='sign-up__page-title' title='Register'/>
-               <SignUpForm onSubmit={this.onSubmit} className="sign-up__form" />
+            <div className='sign-up'>
+                <PageTitle className='sign-up__page-title' title='Register' />
+                <SignUpForm onSubmit={this.onSubmit} className='sign-up__form' />
             </div>
-        );
+        )
     }
 }
 
+SignUp = connect(null, actions)(SignUp);
 
 export default SignUp;
