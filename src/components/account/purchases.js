9f9fc2af314647107e66b7aa9d1895cbcf6aacec
Sean Bass <seanbass99@gmail.com>
2021-07-05 03:05:03 -0500

    implemented set purchase detail action creator and reducer

--- src/components/account/purchases.js
+++ src/components/account/purchases.js
@@ -1,24 +1,20 @@
 import React, { Component } from 'react';
-import * as actions from "../../actions";
-import { connect } from "react-redux";
-
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
 class Purchases extends Component {
-
     componentDidMount() {
         this.props.fetchUserPurchases();
     }
-
     render() {
         const { className } = this.props;
-
         return (
             <div className={`${className} purchases`}>
                 {
                     this.props.purchases.map(purchase => {
                         return (
-                            <div key={purchase._id} className='purchases__purchase purchase'>
-                                <img className="purchase_img" src="http://via.placeholder.com/80x80" alt="" />
-                            </div>
+                            <a onClick={() => this.props.setPurchaseDetail(purchase._id)} key={purchase._id} className='purchases__purchase purchase'>
+                                <img className='purchase__img' src='http://via.placeholder.com/80x80'/>
+                            </a>
                         )
                     })
                 }
@@ -26,12 +22,9 @@ class Purchases extends Component {
         )
     }
 }
-
 function mapStateToProps(state) {
     const { purchases } = state.user;
     return { purchases };
 }
-
 Purchases = connect(mapStateToProps, actions)(Purchases);
-
 export default Purchases;
