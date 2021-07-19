234d705ab2cf34683dac0c191db98e1e0fc25fff
Sean Bass <seanbass99@gmail.com>
2021-07-19 01:11:55 -0500

    structured cart content products component and styled title

--- src/components/shop/shopProduct.js
+++ src/components/shop/shopProduct.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Quantity from '4./quantity';
+import Quantity from '../quantity';
 import GreenPriceTag from '../greenPriceTag';
 class ShopProduct extends Component {
     render() {
